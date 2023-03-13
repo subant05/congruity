501f7864993f75468debe78ed642b7aa391b2a1a
Anthony Crawford <anthonycrawford2@mac.com>
2023-03-13 15:52:11 -0500

    Added asyncMap & asyncChain to Maybe monad

--- src/monad/Maybe.js
+++ src/monad/Maybe.js
@@ -1,87 +1,101 @@
 export default class Maybe {
+  static just(value) {
+    return new Just(value);
+  }
 
-    static just(value){
-        return new Just(value)
-    }
+  static nothing() {
+    return new Nothing();
+  }
 
-    static nothing(){
-        return new Nothing()
-    }
+  static fromNullable(value) {
+    return value != null ? Maybe.just(value) : Maybe.nothing();
+  }
 
-    static fromNullable(value){
-        return  value != null ? Maybe.just(value) : Maybe.nothing()
-    }
-    
-    static of(value){
-        return Maybe.just(value)
-    }
-    
-    get just(){
-        return false;
-    }
+  static of(value) {
+    return Maybe.just(value);
+  }
 
-    get nothing(){
-        return false;
-    }
+  get just() {
+    return false;
+  }
+
+  get nothing() {
+    return false;
+  }
 }
 
-class Just extends Maybe{
-    
-    constructor(value){
-        super()
-        this._value = value;
-    }
+class Just extends Maybe {
+  constructor(value) {
+    super();
+    this._value = value;
+  }
 
-    get value(){
-        return this._value
-    }
+  get value() {
+    return this._value;
+  }
 
-    map(fn){
-       return Maybe.fromNullable(fn(this._value))
-    }
+  map(fn) {
+    return Maybe.fromNullable(fn(this._value));
+  }
 
-    filter(fn){
-        return Maybe.fromNullable(fn(this._value) ? this._value : null)
-    }
+  async asyncMap(fn) {
+    return await Maybe.fromNullable(await fn(this._value));
+  }
 
-    chain(fn){
-        return fn(this._value)
-    }
+  filter(fn) {
+    return Maybe.fromNullable(fn(this._value) ? this._value : null);
+  }
 
-    getOrElse(){
-        return this._value;
-    }
+  chain(fn) {
+    return fn(this._value);
+  }
 
-    toString() {
-        return `Just: ${this._value}`;
-    }
+  async asyncChain(fn) {
+    return await fn(this._value);
+  }
+
+  getOrElse() {
+    return this._value;
+  }
+
+  toString() {
+    return `Just: ${this._value}`;
+  }
 }
 
 class Nothing extends Maybe {
-    constructor(){
-        super()
-    }
-    map(){
-        return this;
-    }
+  constructor() {
+    super();
+  }
+  map() {
+    return this;
+  }
 
-    filter(){
-        return this;
-    }
+  filter() {
+    return this;
+  }
 
-    chain(){
-        return this;
-    }
+  chain() {
+    return this;
+  }
 
-    get(){
-        throw new Error("Can not get value of null or Undefined")
-    }
-    
-    getOrElse(other){
-        return other
-    }
+  async asyncChain() {
+    return this;
+  }
 
-    toString() {
-        return `Nothing: null`;
-    }
+  get value() {
+    throw new Error("Can not get value of null or Undefined");
+  }
+
+  getOrElse(other) {
+    return other;
+  }
+
+  async asyncGetOrElse(fn) {
+    return other;
+  }
+
+  toString() {
+    return `Nothing: null`;
+  }
 }
